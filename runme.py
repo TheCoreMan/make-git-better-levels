d78f4c5dfe2f9b731c2722b388d994a782c4520d
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-01 22:28:53 +0300

    Fixed some stuff in the script, but I'll finish working on it in a different branch...

--- runme.py
+++ runme.py
@@ -2,7 +2,7 @@
 
 def print_something_cool():
     # I'll finish the rest of the work in another branch... zZzZ
-    raise NotImplemented("I'll implement the rest of the work in the steek-shabandar-taenifuge branch.")
+    raise NotImplementedError("I'll implement the rest of this script in the steek-shabandar-taenifuge branch.")
 
 def main():
     print_something_cool()
