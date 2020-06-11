5a575d9f885c37868db31915870b7f2f2c65ea51
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-11 22:13:25 +0300

    Added basic script functionality, added temporary resource.

--- runme.py
+++ runme.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import glob
+import base64
+
+def print_all_resources():
+    # Find all paths in the "script_resources" directory
+    for resource_path in glob.glob("./runme_resources/*"):
+        # Read the resource, decode it, and print the contents
+        with open(resource_path, "r") as resource_file:
+            print("Resource " + resource_path + ":")
+            resource = resource_file.read()
+            decoded_resource = base64.b64decode(resource).decode()
+            print(decoded_resource)
+
 def main():
     print("Script started")
+    print_all_resources()
 
 if __name__ == "__main__":
     main()
