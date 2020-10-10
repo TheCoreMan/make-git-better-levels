061f4c48ce1644a3106b52834f060d5d0f603a03
Shay Nehmad <dude500@gmail.com>
2020-10-10 20:04:48 +0300

    Added a helpful print ヾ(＾∇＾)

--- capture_the_flag.py
+++ capture_the_flag.py
@@ -3,7 +3,7 @@ flag_file_path = "[REDACTED]"
 def main():
     print("Capturing a flag! How exciting.")
     with open(flag_file_path, "r") as flagfile:
-        print(flagfile.read())
+        print("Flag contents are " + flagfile.read())
 
 
 if __name__ == "__main__":
